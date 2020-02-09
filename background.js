58fc269569c0a53faddb7eb7f90a08a5c27155a0
sosunnyproject <parksosun1103@gmail.com>
2020-02-09 22:03:03 +0900

    edit

--- background.js
+++ background.js
@@ -1,28 +1,34 @@
-// https://unsplash.com/s/photos/animals?orientation=landscape
-
-
 const body = document.querySelector("body");
-
-const IMG_LENGTH = 11; // num of images saved in images folder
+const bgChange = document.getElementById("bgChange");
+const currentImg = document.getElementsByClassName("bgImage");
+// const currentImg = document.querySelector("img");
+const IMG_LENGTH = 22; // num of images saved in images folder
 
 function paintImage(imgNumber) {
     const image = new Image();
-    image.src = `images/img${imgNumber+1}.jpg`;
-    console.log(image.src);
+    // const imgName = `images/img${imgNumber+1}` + "^([\\S]+(\\.(?i)(jpg|png|jpeg))$)";
+    // console.log(imgName);
+    image.src = `images/graffiti/img${imgNumber+1}.jpg`;
     image.classList.add("bgImage");
     body.appendChild(image);
 }
-
 
 function genRandom(){
     const num = Math.floor(Math.random() * IMG_LENGTH);
     return num;
 }
 
+function changeImg(){
+  body.removeChild(currentImg[0]);
+  const randomNumber = genRandom();
+  paintImage(randomNumber);
+}
 
 function init(){
     const randomNumber = genRandom();
     paintImage(randomNumber);
 }
+
+bgChange.addEventListener("click", changeImg);
 
 init();
